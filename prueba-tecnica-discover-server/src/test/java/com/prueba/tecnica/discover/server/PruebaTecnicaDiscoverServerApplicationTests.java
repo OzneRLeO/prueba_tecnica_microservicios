b6086f1ddd650861e615ac21bbf6156d51010252
OzneRLeO <87546032+OzneRLeO@users.noreply.github.com>
2024-04-10 15:43:05 -0500

    Add files via upload

---
+++ prueba-tecnica-discover-server/src/test/java/com/prueba/tecnica/discover/server/PruebaTecnicaDiscoverServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prueba.tecnica.discover.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaTecnicaDiscoverServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
