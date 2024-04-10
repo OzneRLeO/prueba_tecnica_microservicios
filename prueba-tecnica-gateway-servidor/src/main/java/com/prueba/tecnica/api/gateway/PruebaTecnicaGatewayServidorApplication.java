39cafacb42b8052fb28f609a3316c699adca05ff
OzneRLeO <87546032+OzneRLeO@users.noreply.github.com>
2024-04-10 15:39:47 -0500

    Add files via upload

---
+++ prueba-tecnica-gateway-servidor/src/main/java/com/prueba/tecnica/api/gateway/PruebaTecnicaGatewayServidorApplication.java
@@ -0,0 +1,13 @@
+package com.prueba.tecnica.api.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaTecnicaGatewayServidorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaTecnicaGatewayServidorApplication.class, args);
+	}
+
+}
---
+++ prueba-tecnica-gateway-servidor/src/test/java/com/prueba/tecnica/api/gateway/PruebaTecnicaGatewayServer3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prueba.tecnica.api.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaTecnicaGatewayServer3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
